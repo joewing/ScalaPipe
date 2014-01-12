05cfdcb10ddde987b9634d279ae7ccc4dae204b9
Joe Wingbermuehle <joewing@joewing.net>
2014-01-12 11:59:06 -0600

    Convert more optimization tests to ScalaTest.

---
+++ src/test/scala/autopipe/opt/StateCompressionSpec.scala
@@ -0,0 +1,94 @@
+package autopipe.opt
+
+import autopipe._
+import autopipe.dsl._
+
+class StateCompressionSpec extends PassTestSpec {
+
+    "StateCompression" should "compress the state space" in {
+
+        val block = new IRBuilder {
+    
+            val in = input(SIGNED32)
+            val out = output(SIGNED32)
+    
+            val x = state(SIGNED32)
+            val y = state(SIGNED32)
+            val z = state(SIGNED32)
+    
+            label(0)
+            start
+    
+            label(1)
+            op(NodeType.assign, x, in)
+    
+            label(2)
+            op(NodeType.add, y, x, literal(1))
+    
+            label(3)
+            op(NodeType.add, z, x, literal(1))
+    
+            label(4)
+            op(NodeType.add, z, z, literal(1))
+    
+            label(5)
+            op(NodeType.assign, out, z)
+    
+            label(6)
+            op(NodeType.assign, x, in)
+    
+            label(7)
+            op(NodeType.assign, y, x)
+    
+            label(8)
+            op(NodeType.assign, z, y)
+    
+            label(9)
+            op(NodeType.assign, out, z)
+    
+        }
+    
+        val expected = new IRBuilder {
+    
+            val in = input(SIGNED32)
+            val out = output(SIGNED32)
+    
+            val x = state(SIGNED32)
+            val y = state(SIGNED32)
+            val z = state(SIGNED32)
+    
+            label(0)
+            start
+    
+            label(1)
+            op(NodeType.assign, x, in)
+    
+            label(2)
+            op(NodeType.add, z, x, literal(1))
+            op(NodeType.add, y, x, literal(1))
+    
+            label(4)
+            op(NodeType.add, z, z, literal(1))
+    
+            label(5)
+            op(NodeType.assign, out, z)
+    
+            label(6)
+            op(NodeType.assign, x, in)
+    
+            label(7)
+            op(NodeType.assign, y, x)
+    
+            label(8)
+            op(NodeType.assign, z, y)
+    
+            label(9)
+            op(NodeType.assign, out, z)
+    
+        }
+
+        checkPass(block, expected, StateCompression)
+
+    }
+
+}
