83b1113e2787d1a074f57d5f7f69d517e08cbbea
Joe Wingbermuehle <joewing@joewing.net>
2014-01-12 19:51:00 -0600

    Fix unit test.

--- src/test/scala/autopipe/ConstantFolderSpec.scala
+++ src/test/scala/autopipe/ConstantFolderSpec.scala
@@ -4,21 +4,24 @@ class ContantFolderSpec extends UnitSpec {
 
     "Folding contant integer expressions" should "create integers" in {
 
-        def check(expected: Int, op: NodeType.Value, a: Int, b: Int = 0) {
-            val a = IntLiteral(2, null)
-            val b = IntLiteral(3, null)
-            val c = IntLiteral(0, null)
+        def check(expected: Int, op: NodeType.Value, ia: Int, ib: Int = 0) {
+            val a = IntLiteral(ia, null)
+            val b = IntLiteral(ib, null)
             val dest = new ASTSymbolNode("dest")
             val expression = ASTOpNode(op, a, b)
             val statement = ASTAssignNode(dest, expression)
             val folder = new ConstantFolder(null)
-            val actual = folder.fold(statement)
+            val actual = folder.fold(statement).asInstanceOf[ASTAssignNode].src
             assert(actual.isInstanceOf[IntLiteral])
             val lit = actual.asInstanceOf[IntLiteral]
-            assert(lit.value == expected)
+            if(lit.value != expected) {
+                fail("got " + lit.value.toString + " expected " + expected)
+            }
         }
 
         check(-5, NodeType.neg, 5)
+        check(~5, NodeType.compl, 5)
+        check(4, NodeType.and, 6, 12)
 
     }
 
